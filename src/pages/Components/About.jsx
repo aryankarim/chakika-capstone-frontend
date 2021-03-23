338ed0ec35ee280b6554e11f29fdfd949e7e5dc2
Aryan Majeed <aryanmajid97@gmail.com>
2021-03-23 17:24:48 +0300

    meaningful names has been added

--- src/pages/Components/About.jsx
+++ src/pages/Components/About.jsx
@@ -4,14 +4,14 @@ import aboutimage from '../images/hero.png'
 function About() {
   return (
     <div className="about">
-        <div className="about-model">
-            <img src={aboutimage} alt="about image" />
-        </div>
-        <div className="about-text">
-            <h2>What is Chakika?</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <button>Learn More</button>
-        </div>
+      <div className="about-model">
+        <img src={aboutimage} alt="about graphics" />
+      </div>
+      <div className="about-text">
+        <h2>What is Chakika?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button>Learn More</button>
+      </div>
     </div>
   );
 }
--- src/pages/Components/Search.jsx
+++ src/pages/Components/Search.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function Search() {
   return (
     <div className="contact">
-        <h3>Search For Your Need</h3>
-        <div className="contact-input">
-            <input type="text" placeholder="Search Here ..." name="search" />
-            <a href="#">Search</a>
-        </div>
+      <h3>Search For Your Need</h3>
+      <div className="contact-input">
+        <input type="text" placeholder="Search Here ..." name="search" />
+        <a href="#">Search</a>
+      </div>
     </div>
   );
 }
