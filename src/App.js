a0e6223d23ad67cabbd484e0812d1acfaf4ce1dd
Aryan Majeed <aryanmajid97@gmail.com>
2021-03-30 01:21:54 +0300

    authentication is added to auth and removed dashboard

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginSigninHeader from "./pages/LoginSigninHeader";
 import ProtectedRoute from "./pages/protectedRoute";
-import Dashboard from "./pages/Dashboard";
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Dashboard} exact />
-        <ProtectedRoute path="/home" component={Home} exact />
+        <ProtectedRoute path="/" component={Home} exact />
         <Route path="/login" component={LoginSigninHeader} exact />
         <Route path="*" render={() => (<div id="notfound">NOT FOUND 404</div>)} />
       </Switch>
