9e723b979c1b14deed168a6d86e6530d707e122d
shelgir <shelgirr@gmail.com>
2021-04-05 16:21:34 +0300

    Account and Contact Page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginSigninHeader from "./pages/LoginSigninHeader";
 import Searchp from "./pages/Searchp";
+import Account from "./pages/Account";
+import Contact from "./pages/Contact";
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" component={LoginSigninHeader} exact />
         <Route path="/Home" component={Home} exact />
         <Route path="/Searchp" component={Searchp} exact />
+        <Route path="/Account" component={Account} exact />
+        <Route path="/Contact" component={Contact} exact />
       </Switch>
     </BrowserRouter >
   );
--- src/pages/Components/Navbar.jsx
+++ src/pages/Components/Navbar.jsx
@@ -5,19 +5,16 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-        <a href="../Home" className="logo">
-            <img src={logo} alt="logo" />
-        </a>
-        <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label className="menu-icon" for="menu-btn">
-            <span className="nav-icon"></span>
-        </label>
+        <div className="menu-icon" for="menu-btn">
+            <a href="../Home" className="logo">
+              <img src={logo} alt="logo" />
+            </a>
+        </div>
         <ul className="menu">
             <li><a href="../Home">Home</a></li>
             <li><a href="#">Garage</a></li>
-            <li><a href="#">Categories</a></li>
-            <li><a href="#">Account</a></li>
-            <li><a href="#">About Us</a></li>
+            <li><a href="../Account">Account</a></li>
+            <li><a href="../Contact">Contact Us</a></li>
             <li><a href="../Searchp">Search</a></li>
         </ul>
         
