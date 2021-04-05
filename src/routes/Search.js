13fee67d0d3b898106cd4b98478853a9e3a4b0d9
Aryan Majeed <aryanmajid97@gmail.com>
2021-04-05 16:51:01 +0300

    search and file structure are done

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -2,7 +2,6 @@ import React from 'react'
 import SearchSection from './Components/SearchSection';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-import SearchFun from './pages/SearchFun';
 import '../styles/home.css';
 
 export default function Search() {
@@ -10,7 +9,6 @@ export default function Search() {
         <div className="s-container">
             <Navbar />
             <SearchSection />
-            <SearchFun />
             <Footer />
         </div>
     )
